7f086482b5f0641c22ad59143c699dfe51ff3afd
ealves_noot <endvan@gmail.com>
2025-05-01 12:49:56 -0300

    pesquisa clientes

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -17,7 +17,17 @@ export class ClienteService {
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
   }
 
-  obterStorage(): Cliente[] {
+  pesquisaClientes(nomeBusca: string): Cliente[] {
+    const clientes = this.obterStorage();
+    if (!nomeBusca) {
+      return clientes
+    }
+
+    return clientes.filter(cliente => cliente.nome?.indexOf(nomeBusca) !== -1);
+
+  }
+
+  private obterStorage(): Cliente[] {
     const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
     if (repositorioClientes) {
       return JSON.parse(repositorioClientes);
--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {FormsModule} from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
@@ -6,13 +6,52 @@ import {MatIconModule} from '@angular/material/icon';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatInputModule} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
+import {ClienteService} from '../cliente.service';
+import {Cliente} from '../cadastro/cliente';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'}
+];
+
 
 @Component({
   selector: 'app-consulta',
-  imports: [MatCardModule, FormsModule, MatTableModule, MatIconModule, FlexLayoutModule, MatInputModule, MatButton],
+  standalone: true,
+  imports: [MatCardModule,
+    FormsModule,
+    MatTableModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatButton
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+  nomeBusca: string = '';
+  colunasTable: string[] = ['id', 'nome', 'dataNascimento', 'email'];
+  listaClientes: Cliente[] = [];
+
+  constructor(private service: ClienteService) {
+  }
+
+  ngOnInit(): void {
+    this.listaClientes = this.service.pesquisaClientes('');
+  }
+
+  pesquisar() {
+    this.listaClientes = this.service.pesquisaClientes(this.nomeBusca);
+    console.log(this.listaClientes,"pesquisa")
+  }
 
 }
