261cb20226a9b268bbd8d966da2c6ae254d81531
Ender <endvan@gmail.com>
2025-05-02 20:04:28 -0300

    config routas modulo layout separado

--- passeio-app1/src/app/app-routing.module.ts
+++ passeio-app1/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+// loadChildren => configura todas as rotas de um module
+const routes: Routes = [
+  {
+    path: 'paginas',
+    loadChildren: () => import('./template/template.module')
+      .then(m => m.TemplateModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
