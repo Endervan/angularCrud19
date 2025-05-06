95383f8f2960925ffdf11945a379cb84c83f1e03
Ender <endvan@gmail.com>
2025-05-06 01:22:28 -0300

    docker config imagem e container da api

--- passeio-app1/src/app/lugares/lugares.service.ts
+++ passeio-app1/src/app/lugares/lugares.service.ts
@@ -2,34 +2,37 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Lugar} from './lugar/lugar';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugaresService {
 
+  apiUrl: string = environment.apiUrl + '/lugares';
+
   constructor(private readonly http: HttpClient) {
   }
 
   salvar(lugar: Lugar): Observable<Lugar> {
-    return this.http.post<Lugar>("http://localhost:3000/lugares", lugar)
+    return this.http.post<Lugar>(this.apiUrl, lugar)
   }
 
   obterTodas(): Observable<Lugar[]> {
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares')
+    return this.http.get<Lugar[]>(this.apiUrl)
   }
 
   filtar(nome: string, categoria: string): Observable<Lugar[]> {
     let parametros = new HttpParams();
 
     if (nome) {
-      parametros =  parametros.set('nome_like', nome)
+      parametros = parametros.set('nome_like', nome)
     }
 
     if (categoria && categoria !== '-1') {
-      parametros =  parametros.set('categoria', categoria)
+      parametros = parametros.set('categoria', categoria)
     }
-    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+    return this.http.get<Lugar[]>(this.apiUrl, {
         params: parametros
       }
     )
